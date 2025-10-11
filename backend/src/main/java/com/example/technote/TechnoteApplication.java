15d0b9aeb1e2e1db84e9ffe4afc6b5748d14d2d7
naya1228 <jin100y43@gmail.com>
2025-10-11 10:08:11 +0900

    백엔드 고친거 커밋

--- backend/src/main/java/com/example/technote/TechnoteApplication.java
+++ backend/src/main/java/com/example/technote/TechnoteApplication.java
@@ -1,4 +1,4 @@
-package com.example.technote.controller;
+package com.example.technote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
