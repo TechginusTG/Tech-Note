1e71012b722c58dbed2fd66a2a74e081bcfaedfd
naya1228 <jin100y43@gmail.com>
2025-10-28 20:53:05 +0900

    /apt/users/[id] 추가됨

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,50 +1,27 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import bcrypt from "bcrypt";
 
-export async function PUT(
+export async function GET(
   request: Request,
-  context: { params: { id: string } },
+  { params }: { params: { id: string } },
 ) {
-  const session = await getServerSession(authOptions);
-  if (!session || !session.user?.email) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
-
-  const { id } = context.params;
-  if (parseInt(id, 10) !== session.user.id) {
-    return NextResponse.json({ error: "Forbidden" }, { status: 403 });
-  }
-
+  const { id } = await params;
   try {
-    const body = await request.json();
-    const { name, password, profilePicture } = body;
-
-    const dataToUpdate: any = {};
-
-    if (name) {
-      dataToUpdate.name = name;
-    }
-
-    if (password && password.length > 0) {
-      const hashedPassword = await bcrypt.hash(password, 10);
-      dataToUpdate.password = hashedPassword;
-    }
-
-    if (profilePicture) {
-      dataToUpdate.image = profilePicture;
-    }
-
-    const updatedUser = await prisma.user.update({
-      where: { id: id },
-      data: dataToUpdate,
+    const user = await prisma.user.findUnique({
+      where: {
+        id: id,
+      },
     });
 
-    return NextResponse.json(updatedUser);
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+    return NextResponse.json(user);
   } catch (error) {
-    console.error(`Error updating user ${id}:`, error);
-    return NextResponse.json({ error: "Error updating user" }, { status: 500 });
+    console.error("Error fetching user", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
   }
 }
