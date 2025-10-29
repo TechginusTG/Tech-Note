09a4d45fbd2461447a6731ea993147be3980ccca
system0105 <songsy200701@gmail.com>
2025-10-29 17:50:34 +0900

    버그 고치다가 api 다 씀 디자인, 배치 구성예정

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -4,7 +4,7 @@ import prisma from "@/lib/prisma";
 export async function POST(request: NextRequest, response: NextResponse) {
   try {
     const body = await request.json();
-    const { title, content, author, attachments };
+    const { title, content, author, attachments } = body;
   } catch (e) {
     console.error("Couldn't post this post", e);
     return response.status(500).json({ error: "Couldn't post this post" });
